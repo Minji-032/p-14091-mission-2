03b9b3936394148a4ae603bb2e42a710935ea35e
Minji-032 <5h6vmm@gmail.com>
2025-11-28 16:32:19 +0900

    3장 완료

---
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,50 @@
+package com.mysite.sbb.question;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.user.SiteUser;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDateTime createDate;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Answer> answers = new ArrayList<>();
+
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
+
+    public Answer addAnswer(String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setQuestion(this);
+        answer.setCreateDate(LocalDateTime.now());
+        answers.add(answer);
+
+        return answer;
+    }
+}
